9a19a553c9cb7a674264c166e79867db05a0aeeb
EhDohWah <39404722+EhDohWah@users.noreply.github.com>
2019-09-18 22:09:38 +0700

    adding @ech %random% command

--- Fprogram.bat
+++ Fprogram.bat
@@ -2,5 +2,7 @@
 color a
 mode 300
 :Start
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+goto start
 ping 8.8.8.8 -t
 
